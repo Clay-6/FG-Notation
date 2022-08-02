08a2bee14b4a890fe883e50d0be338939ae335e4
Clayton <claytonwambu@gmail.com>
2022-08-02 19:36:38 +0100

    Completed `Button` impls

--- src/abbreviated.rs
+++ src/abbreviated.rs
@@ -1,3 +1,8 @@
+use core::fmt;
+use std::str::FromStr;
+
+use crate::CreationError;
+
 pub struct Move {
     button: Button,
     motion: Motion,
@@ -33,4 +38,32 @@ pub enum Modifier {
     Jump,
     JumpCancel,
     TigerKnee,
+}
+
+impl Button {
+    pub fn from<S>(b: S) -> Result<Self, CreationError>
+    where
+        S: ToString,
+    {
+        let b = b.to_string();
+        if !b.chars().all(|c| c.is_ascii_alphabetic()) {
+            Err(CreationError::InvalidButton)
+        } else {
+            Ok(Self(b))
+        }
+    }
+}
+
+impl FromStr for Button {
+    type Err = CreationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from(s)
+    }
+}
+
+impl fmt::Display for Button {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
 }
