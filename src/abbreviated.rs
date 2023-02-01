1cfa673bd5c48db9b4c903a202b5b38129808112
Clay <claytonwambu@gmail.com>
2023-02-01 20:13:23 +0000

    Use pretty_assertions in tests

--- src/abbreviated.rs
+++ src/abbreviated.rs
@@ -315,6 +315,7 @@ impl fmt::Display for Button {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn qcf_hp() {
--- src/numpad.rs
+++ src/numpad.rs
@@ -286,6 +286,7 @@ impl FromStr for Modifier {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     #[test]
     fn j236h() {
